3c66c7f36f1151b25f1f6db12b434a2b86aa4c84
IMHOJEONG <39ghwjd@naver.com>
2019-12-25 23:00:20 +0900

    switch.sh에 오류 다시 수정 ' ' => " "

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,7 +9,7 @@ function switch_proxy() {
 
     echo "> 전환할 port: $IDLE_PORT"
     echo "> Port 전환"
-    echo 'set \$service_url http://127.0.0.1:${IDLE_PORT};' | sudo tee /etc/nginx/conf.d/service-url.inc
+    echo "set $service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
     # ec2 인스턴스에서 service-url.inc 파일 오타 주의
 
     echo "> 엔진엑스 Reload"
